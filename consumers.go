d299904b9c163594df753aac644a5f6695210ffa
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-06-17 10:44:43 +0300

    note origin

--- consumers.go
+++ consumers.go
@@ -13,6 +13,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Taken from: https://github.com/stealthly/go_kafka_client/blob/master/consumers/consumers.go
+ *
  */
 
 package main
