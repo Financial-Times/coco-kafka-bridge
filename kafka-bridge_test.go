dce94b5d58951a4c8c2e1bddb2cc5dd940c5c9fe
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-06-18 16:26:26 +0300

    changed test structure

--- kafka-bridge_test.go
+++ kafka-bridge_test.go
@@ -1,27 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
     "testing"
 )
 
 func TestExtractJSON(t *testing.T) {
 
-    buf, err := ioutil.ReadFile("test_msg.txt")
-    if (err != nil) {
-        fmt.Print("Couldn't read content from file.")
-    }
-    msg := string(buf)
+    var tests = []struct {
+        kafkaMsg    string
+        expectedJsonContent string
+    } {
+        {
+            `
+            FTMSG/1.0
+            Message-Id: bb07b9ab-0ff6-4853-bdd1-104906d7d282
+            Message-Timestamp: 2015-06-17T12:16:39.022Z
+            Message-Type: cms-content-published
+            Origin-System-Id: http://cmdb.ft.com/systems/methode-web-pub
+            Content-Type: application/json
+            X-Request-Id: tid_6y3oogjqhk
 
-    buf, err = ioutil.ReadFile("test_content.txt")
-    if (err != nil) {
-        fmt.Print("Couldn't read content from file.")
-    }
-    expectedContent := string(buf)
+            { "uuid":"f9d6eecc-14b4-11e5-973e-a0f360779259","type":"EOM::CompoundStory","value":"bodor_kafka_bridge_test","attributes":[],"linkedObjects":[] }
+            `,
+            `{ "uuid":"f9d6eecc-14b4-11e5-973e-a0f360779259","type":"EOM::CompoundStory","value":"bodor_kafka_bridge_test","attributes":[],"linkedObjects":[] }`,
 
-    actualContent := extractJSON(msg);
-    if (expectedContent != actualContent) {
-        t.Errorf("not equal")
+        },
+    }
+
+    for _, test := range tests {
+        actualJSONContent := extractJSON(test.kafkaMsg);
+        if (test.expectedJsonContent != actualJSONContent) {
+            t.Errorf("not equal")
+        }
     }
 }
