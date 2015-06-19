e3f7779c2368d1ff4f65a51ad3255a210538cec4
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-06-19 16:59:15 +0300

    added puppet module

---
+++ puppet/ft-kafka_bridge/manifests/init.pp
@@ -0,0 +1,43 @@
+class kafka_bridge {
+
+  $binary_name = "kafka-bridge"
+  $install_dir = "/usr/local/$binary_name"
+  $binary_file = "$install_dir/$binary_name"
+  $log_dir = "/var/log/apps"
+  $config_file = "/etc/$binary_name.properties"
+
+  class { 'common_pp_up': }
+  class { "${module_name}::supervisord": }
+
+  file {
+    $install_dir:
+      mode    => "0664",
+      ensure  => directory;
+
+    $binary_file:
+      ensure  => present,
+      source  => "puppet:///modules/$module_name/$binary_name",
+      mode    => "0755",
+      require => File[$install_dir];
+
+    $config_file:
+      content => template("$module_name/kafka-bridge.properties.erb"),
+      mode    => "0664";
+
+    $log_dir:
+      ensure  => directory,
+      mode    => "0664"
+  }
+
+  exec { 'restart_nativerw':
+    command     => "supervisorctl restart $binary_name",
+    path        => "/usr/bin:/usr/sbin:/bin",
+    subscribe   => [
+      File[$binary_file],
+      File[$config_file],
+      Class["${module_name}::supervisord"]
+    ],
+    before      => Class["${module_name}::monitoring"],
+    refreshonly => true
+  }
+}
---
+++ puppet/scripts/deploy-to-forge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/sbin:/bin
+
+start_time=`date +%s`
+MODULEFULLPATH="$WORKSPACE/puppet/ft-kafka_bridge"
+
+/usr/local/bin/forge-admin.py --publish --source "$MODULEFULLPATH"
+ERROR_CODE=$?
+if [[ $ERROR_CODE -ne 0 ]]; then
+    echo -e "Attempt to publish $MODULEFULLPATH failed with code $ERROR_CODE.\n"
+    exit 255
+fi
+echo ""
+exit 0
