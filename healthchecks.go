6f92b9a525eabacf4000b44d8759ef927bc4a475
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-08-05 11:29:12 +0300

    added panic guide url

--- healthchecks.go
+++ healthchecks.go
@@ -12,7 +12,7 @@ func (bridge BridgeApp) ForwardHealthcheck() fthealth.Check {
 	return fthealth.Check{
 		BusinessImpact:   "Forwarding messages to coco cluster won't work. Publishing in the containerised stack won't work.",
 		Name:             "Forward to aws co-co cluster",
-		PanicGuide:       "none",
+		PanicGuide:       "https://sites.google.com/a/ft.com/ft-technology-service-transition/home/run-book-library/kafka-bridge-run-book",
 		Severity:         1,
 		TechnicalSummary: "Forwarding messages is broken. Check networking, aws cluster reachability and/or coco cms-notifier state.",
 		Checker:          bridge.checkForwardable,
@@ -39,7 +39,7 @@ func (bridge BridgeApp) ConsumeHealthcheck() fthealth.Check {
 	return fthealth.Check{
 		BusinessImpact:   "Consuming messages from kafka won't work. Publishing in the containerised stack won't work.",
 		Name:             "Consume from kafka",
-		PanicGuide:       "none",
+		PanicGuide:       "https://sites.google.com/a/ft.com/ft-technology-service-transition/home/run-book-library/kafka-bridge-run-book",
 		Severity:         1,
 		TechnicalSummary: "Consuming messages is broken. Check kafka/zookeeper is reachable.",
 		Checker:          bridge.checkConsumable,
