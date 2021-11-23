4b06866f1df7133f1388a3dfbe2fc8c4b34caa2e
Stephan Zahariev <23424829+StephanZahariev@users.noreply.github.com>
2021-11-23 17:33:32 +0200

    Propagate schema-version

--- plain_http_producer_test.go
+++ plain_http_producer_test.go
@@ -41,7 +41,7 @@ func TestSendMessage(t *testing.T) {
 				"X-Request-Id":       "t9happe59y",
 				"Authorization":      "authorizationkey",
 				"Message-Timestamp":  "2015-07-06T07:03:09.362Z",
-				"Content-Type":      "application/json",
+				"Content-Type":       "application/json",
 			},
 		},
 		{ //missing content-type
@@ -86,7 +86,7 @@ func TestSendMessage(t *testing.T) {
 				"X-Request-Id":       "t9happe59y",
 				"Authorization":      "",
 				"Message-Timestamp":  "2015-07-06T07:03:09.362Z",
-				"Content-Type":      "application/json",
+				"Content-Type":       "application/json",
 			},
 		},
 		{ //host header (queue) is missing
@@ -109,7 +109,7 @@ func TestSendMessage(t *testing.T) {
 				"X-Request-Id":       "t9happe59y",
 				"Authorization":      "",
 				"Message-Timestamp":  "2015-07-06T07:03:09.362Z",
-				"Content-Type":      "application/json",
+				"Content-Type":       "application/json",
 			},
 		},
 		{ //origin system id is missing
@@ -131,7 +131,7 @@ func TestSendMessage(t *testing.T) {
 				"X-Request-Id":       "t9happe59y",
 				"Authorization":      "",
 				"Message-Timestamp":  "2015-07-06T07:03:09.362Z",
-				"Content-Type":      "application/json",
+				"Content-Type":       "application/json",
 			},
 		},
 		{ // origin system id is invalid (but the bridge shouldn't care)
@@ -155,7 +155,7 @@ func TestSendMessage(t *testing.T) {
 				"X-Request-Id":       "t9happe59y",
 				"Authorization":      "authorizationkey",
 				"Message-Timestamp":  "2015-07-06T07:03:09.362Z",
-				"Content-Type":      "application/json",
+				"Content-Type":       "application/json",
 			},
 		},
 		{ //Message-Timestamp is missing
@@ -177,7 +177,7 @@ func TestSendMessage(t *testing.T) {
 				"X-Request-Id":       "t9happe59y",
 				"Authorization":      "",
 				"Message-Timestamp":  "",
-				"Content-Type":      "application/json",
+				"Content-Type":       "application/json",
 			},
 		},
 		{ //native-hash forward
@@ -203,7 +203,35 @@ func TestSendMessage(t *testing.T) {
 				"Authorization":      "authorizationkey",
 				"Message-Timestamp":  "2015-07-06T07:03:09.362Z",
 				"X-Native-Hash":      "27f79e6d884acdd642d1758c4fd30d43074f8384d552d1ebb1959345",
-				"Content-Type":      "application/json",
+				"Content-Type":       "application/json",
+			},
+		},
+		{ //schema-version forward
+			queueProducer.MessageProducerConfig{
+				Addr:          "address",
+				Authorization: "authorizationkey",
+			},
+			"",
+			queueProducer.Message{
+				Headers: map[string]string{
+					"Message-Id":        "fc429b46-2500-4fe7-88bb-fd507fbaf00c",
+					"Message-Timestamp": "2015-07-06T07:03:09.362Z",
+					"Message-Type":      "cms-content-published",
+					"Origin-System-Id":  "http://cmdb.ft.com/systems/methode-web-pub",
+					"Content-Type":      "application/json",
+					"X-Request-Id":      "t9happe59y",
+					"Native-Hash":       "27f79e6d884acdd642d1758c4fd30d43074f8384d552d1ebb1959345",
+					"X-Schema-Version":  "2",
+				},
+				Body: `{"uuid":"7543220a-2389-11e5-bd83-71cb60e8f08c","type":"EOM::CompoundStory","value":"test"}`},
+			map[string]string{
+				"X-Origin-System-Id": "http://cmdb.ft.com/systems/methode-web-pub",
+				"X-Request-Id":       "t9happe59y",
+				"Authorization":      "authorizationkey",
+				"Message-Timestamp":  "2015-07-06T07:03:09.362Z",
+				"X-Native-Hash":      "27f79e6d884acdd642d1758c4fd30d43074f8384d552d1ebb1959345",
+				"Content-Type":       "application/json",
+				"X-Schema-Version":   "2",
 			},
 		},
 	}
